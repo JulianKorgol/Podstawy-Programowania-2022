37f85681f19ee3a523da31d2cecd170129394ba2
Julian Korgol <u25_julkor_waw@technischools.com>
2021-12-14 11:35:47 +0100

    2021-12-14 -> Dodatkowy projekt.

--- Sekurak - CTF - pitagoras - not completed.py
+++
@@ -1,6 +0,0 @@
-import math
-number = 137418153209
-obliczenie = (number/2)
-obliczeniepierwiastek = math.sqrt(obliczenie)
-wynik = int(obliczeniepierwiastek)
-print(wynik)
--- main.py
+++ main.py
@@ -1,39 +1,19 @@
-# Zad 1
+firstWord = input()
+secondWord = input()
 
-# import math
-#
-# liczba = int(input("Podaj liczbę: "))
-# pierwiastek = int(math.sqrt(liczba))
-#
-# isPrime = True
-# for i in range(2, pierwiastek+1):
-#     if (liczba%i)==0:
-#         isPrime = False
-#         break
-# if isPrime:
-#     print('Jest pierwsza.')
-# else:
-#     print("Nie jest pierwsza.")
+def merge(fword, sword):
+    fwordLen = int(len(fword))
+    swordLen = int(len(sword))
+    final = []
+    if fwordLen > swordLen:
+        for i in range(swordLen):
+            final.append(fword[i])
+            final.append(sword[i])
+    else:
+        for i in range(fwordLen):
+            final.append(fword[i])
+            final.append(sword[i])
+    finalConverted = ''.join(final)
+    return finalConverted
 
-# Zad 2
-import math
-
-liczba = int(input("Podaj liczbę do której mam szukać: "))
-nowaliczba = liczba*liczba
-tab = [True for i in range (nowaliczba+1)]
-
-def liczbypierwsze(tab, liczba):
-    is_prime = tab
-    is_prime[0] = False
-    is_prime[1] = False
-    for i in range(2, int(math.sqrt(liczba))+1):
-        if tab[i] == True:
-            print(i, end=" ")
-            for j in range(i*i, liczba+1, i):
-                tab[j] = False
-    return tab
-
-liczbypierwsze(tab, nowaliczba)
-
-# Trzeba użyć whila.
-# while n > len(tab)
+print(merge(firstWord, secondWord))
