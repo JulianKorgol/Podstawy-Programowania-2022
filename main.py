4e56581944c9debd33fef9c599fc5fb24b866294
Julian Korgol <kontakt@juliankorgol.com>
2022-02-22 11:10:51 +0100

    kartkowka + insertion sort [2022-02-22]

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-zbior = [10, 8, 7, 9, -3]
-# zbior = [-5, -10, 7, 2]
+ciag = input()
+drugiCiag = ciag[::-1]
 
-def sortowanieBombelkowe(zbior):
-    x = 1
-    for i in range(len(zbior)-1):
-        if zbior[i] > zbior[i+1]:
-            lewa = zbior[i]
-            prawa = zbior[i+1]
-            zbior[i] = prawa
-            zbior[i+1] = lewa
-        for i in range(len(zbior) - x):
-            if zbior[i] > zbior[i + 1]:
-                lewa = zbior[i]
-                prawa = zbior[i + 1]
-                zbior[i] = prawa
-                zbior[i + 1] = lewa
-                x=+1
-    print(zbior)
-
-sortowanieBombelkowe(zbior)
+if ciag == drugiCiag:
+    print('Tak')
+else:
+    print('Nie')
