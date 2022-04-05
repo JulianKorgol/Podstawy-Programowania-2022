0d72971691903c4619f90251f79a22e4fb99f311
Julian Korgol <kontakt@juliankorgol.com>
2022-04-05 11:25:45 +0200

    Bucket sort (nie było mnie)

--- main.py
+++ main.py
@@ -1,52 +1,28 @@
-import math
+#Bucket sort - nie było mnie
+arr = [0.78, 0.17, 0.39, 0.26, 0.72,0.94, 0.21, 0.12, 0.23, 0.68]
+print(arr)
 
-liczbaKlas = int(input())
-dane = ""
-klasy = []
+def bucketSort(tab):
+    najwieksza = max(tab)
+    najmniejsza = min(tab)
+    bucket = []
+    for i in range(len(tab)):
+        bucket.append([])
+    for liczba in tab:
+        index = int(10 * liczba)
+        bucket[index].append(liczba)
+    for liczba2 in range(len(tab)):
+        bucket[liczba2] = sorted(bucket[liczba2])
 
-def merge(arr, l, p):
-    i_l = 0
-    i_p = 0
-    i = 0
-    while i_l < len(l) and i_p < len(p):
-        if l[i_l] < p[i_p]:
-            arr[i] = l[i_l]
-            i_l += 1
-        else:
-            arr[i] = p[i_p]
-            i_p += 1
-        i += 1
-    while i_l < len(l):
-        arr[i] = l[i_l]
-        i += 1
-        i_l += 1
-    while i_p < len(p):
-        arr[i] = p[i_p]
-        i += 1
-        i_p += 1
+    k=0
+    for i in range(len(tab)):
+        for j in range(len(bucket[i])):
+            tab[k] = bucket[i][j]
+            k += 1
+    return tab
 
 
 
-def sortowanie(tab):
-    if len(tab) > 1:
-        s = len(tab)//2
-        left = tab[:s]
-        right = tab[s:]
-        sortowanie(left)
-        sortowanie(right)
-        merge(tab, left, right)
+print(bucketSort(arr))
 
-
-
-# Przyjmowanie danych i podział danych na klasy
-for i in range(liczbaKlas):
-    dane = ""
-    while dane != "-":
-        dane = input()
-        klasy.append(dane.split())
-    posortowane = sortowanie(klasy)
-    dlugoscPosortowane = math.ceil(len(posortowane))/10
-    for i in range(dlugoscPosortowane-1):
-        doPrintu = posortowane[i][0]
-        doPrintu += posortowane[i][1]
-    print("-")
+# https://www.programiz.com/dsa/bucket-sort
